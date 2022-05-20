fb669fd17bf23c3130bbb5584ad444e843ba6252
Rakibul md <rakib245875@gmail.com>
2022-05-21 00:24:29 +0600

    added login page google sign in & added firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h1>This is login page</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold text-secondary">
+                        Login
+                    </h2>
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => {
+                            signInWithGoogle();
+                        }}
+                        className="btn btn-outline"
+                    >
+                        Continue with GOOGLE
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
